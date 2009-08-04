0633737e009c98f99d5f826ba69b018f975ef74e
hugith <hugith@23fad586-80de-11de-b289-772754cc8021>
2009-08-04 10:11:25 +0000

    Added comments

--- trunk/Sources/your/app/components/Main.java
+++ trunk/Sources/your/app/components/Main.java
@@ -4,8 +4,13 @@ import com.webobjects.appserver.WOContext;
 
 import er.extensions.components.ERXComponent;
 
+/**
+ * @author Hugi Thordarson
+ */
+
 public class Main extends ERXComponent {
-	public Main(WOContext context) {
-		super(context);
+
+	public Main( WOContext context ) {
+		super( context );
 	}
 }
